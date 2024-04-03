6811d588739b9317671da6ab6e76e55362a82ce3
seaskyho <seaskyho@gmail.com>
2024-04-03 15:49:40 +1100

    update

---
+++ mytest.py
@@ -0,0 +1,4 @@
+for i in range(4):
+    row = i // 2
+    col = i % 2
+    print(row, col)
--- test.py
+++ test.py
@@ -11,12 +11,19 @@ from skimage import measure
 from tabulate import tabulate
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-from sklearn.metrics import auc, roc_auc_score, average_precision_score, f1_score, precision_recall_curve, pairwise
+from sklearn.metrics import (
+    auc,
+    roc_auc_score,
+    average_precision_score,
+    f1_score,
+    precision_recall_curve,
+    pairwise,
+)
 
 import open_clip
 from few_shot import memory
 from model import LinearLayer
-from dataset import VisaDataset, MVTecDataset
+from dataset import LOCODataset, VisaDataset, MVTecDataset
 from prompt_ensemble import encode_text_with_prompt_ensemble
 
 
@@ -81,23 +88,28 @@ def test(args):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    txt_path = os.path.join(save_path, 'log.txt')
+    txt_path = os.path.join(save_path, "log.txt")
 
     # clip
-    model, _, preprocess = open_clip.create_model_and_transforms(args.model, img_size, pretrained=args.pretrained)
+    model, _, preprocess = open_clip.create_model_and_transforms(
+        args.model, img_size, pretrained=args.pretrained
+    )
     model.to(device)
     tokenizer = open_clip.get_tokenizer(args.model)
+    # TODO: what is model and tokenizer?
 
     # logger
     root_logger = logging.getLogger()
     for handler in root_logger.handlers[:]:
         root_logger.removeHandler(handler)
     root_logger.setLevel(logging.WARNING)
-    logger = logging.getLogger('test')
-    formatter = logging.Formatter('%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s',
-                                  datefmt='%y-%m-%d %H:%M:%S')
+    logger = logging.getLogger("test")
+    formatter = logging.Formatter(
+        "%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s",
+        datefmt="%y-%m-%d %H:%M:%S",
+    )
     logger.setLevel(logging.INFO)
-    file_handler = logging.FileHandler(txt_path, mode='w')
+    file_handler = logging.FileHandler(txt_path, mode="w")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     console_handler = logging.StreamHandler()
@@ -106,111 +118,180 @@ def test(args):
 
     # record parameters
     for arg in vars(args):
-        if args.mode == 'zero_shot' and (arg == 'k_shot' or arg == 'few_shot_features'):
+        if args.mode == "zero_shot" and (arg == "k_shot" or arg == "few_shot_features"):
             continue
-        logger.info(f'{arg}: {getattr(args, arg)}')
+        logger.info(f"{arg}: {getattr(args, arg)}")
 
     # seg
-    with open(args.config_path, 'r') as f:
+    with open(args.config_path, "r") as f:
         model_configs = json.load(f)
-    linearlayer = LinearLayer(model_configs['vision_cfg']['width'], model_configs['embed_dim'],
-                              len(features_list), args.model).to(device)
-    checkpoint = torch.load(args.checkpoint_path)
+    linearlayer = LinearLayer(
+        model_configs["vision_cfg"]["width"],
+        model_configs["embed_dim"],
+        len(features_list),
+        args.model,
+    ).to(device)
+    checkpoint = torch.load(args.checkpoint_path, map_location=device)
     linearlayer.load_state_dict(checkpoint["trainable_linearlayer"])
 
     # dataset
-    transform = transforms.Compose([
+    transform = transforms.Compose(
+        [
             transforms.Resize((img_size, img_size)),
             transforms.CenterCrop(img_size),
-            transforms.ToTensor()
-        ])
-    if dataset_name == 'mvtec':
-        test_data = MVTecDataset(root=dataset_dir, transform=preprocess, target_transform=transform,
-                                 aug_rate=-1, mode='test')
+            transforms.ToTensor(),
+        ]
+    )
+
+    if dataset_name == "mvtec":
+        test_data = MVTecDataset(
+            root=dataset_dir,
+            transform=preprocess,
+            target_transform=transform,
+            aug_rate=-1,
+            mode="test",
+        )
+    elif dataset_name == "loco":
+        # TODO: update for LOCO
+        test_data = LOCODataset(
+            root=dataset_dir,
+            transform=preprocess,
+            target_transform=transform,
+            # aug_rate=-1, TODO: do i need this?
+            mode="test",
+        )
     else:
-        test_data = VisaDataset(root=dataset_dir, transform=preprocess, target_transform=transform, mode='test')
-    test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False)
-    obj_list = test_data.get_cls_names()
+        test_data = VisaDataset(
+            root=dataset_dir,
+            transform=preprocess,
+            target_transform=transform,
+            mode="test",
+        )
+    test_dataloader = torch.utils.data.DataLoader(
+        test_data, batch_size=1, shuffle=False
+    )
+    obj_list = test_data.get_cls_names()  # ['bottle', 'cable', ... ]
 
     # few shot
-    if args.mode == 'few_shot':
-        mem_features = memory(args.model, model, obj_list, dataset_dir, save_path, preprocess, transform,
-                              args.k_shot, few_shot_features, dataset_name, device)
+    if args.mode == "few_shot":
+        # TODO: update for LOCO
+        mem_features = memory(
+            args.model,
+            model,
+            obj_list,
+            dataset_dir,
+            save_path,
+            preprocess,
+            transform,
+            args.k_shot,
+            few_shot_features,
+            dataset_name,
+            device,
+        )
 
     # text prompt
     with torch.cuda.amp.autocast(), torch.no_grad():
-        text_prompts = encode_text_with_prompt_ensemble(model, obj_list, tokenizer, device)
+        # TODO: this takes a lot of time, consider save it
+        # text_prompts = encode_text_with_prompt_ensemble(
+        #     model, obj_list, tokenizer, device
+        # )
+        with open("text_prompts_mvtec.t", "rb") as f:
+            text_prompts = torch.load(f)
 
     results = {}
-    results['cls_names'] = []
-    results['imgs_masks'] = []
-    results['anomaly_maps'] = []
-    results['gt_sp'] = []
-    results['pr_sp'] = []
+    results["cls_names"] = []
+    results["imgs_masks"] = []
+    results["anomaly_maps"] = []
+    results["gt_sp"] = []
+    results["pr_sp"] = []  # prediction of sample(image)'s anomaly score
     for items in test_dataloader:
-        image = items['img'].to(device)
-        cls_name = items['cls_name']
-        results['cls_names'].append(cls_name[0])
-        gt_mask = items['img_mask']
+        image = items["img"].to(device)
+        cls_name = items["cls_name"]
+        results["cls_names"].append(cls_name[0])
+        gt_mask = items["img_mask"]
         gt_mask[gt_mask > 0.5], gt_mask[gt_mask <= 0.5] = 1, 0
-        results['imgs_masks'].append(gt_mask)  # px
-        results['gt_sp'].append(items['anomaly'].item())
+        results["imgs_masks"].append(gt_mask)  # px
+        results["gt_sp"].append(items["anomaly"].item())
 
         with torch.no_grad(), torch.cuda.amp.autocast():
-            image_features, patch_tokens = model.encode_image(image, features_list)
+            image_features, patch_tokens = model.encode_image(
+                image, features_list
+            )  # image_features.shape: [1, 768]; patch_tokens of len==4, each shape [1, 1370, 1024]
             image_features /= image_features.norm(dim=-1, keepdim=True)
             text_features = []
+
+            # cls_name is just one item list, e.g., ["bottle"]
             for cls in cls_name:
                 text_features.append(text_prompts[cls])
-            text_features = torch.stack(text_features, dim=0)
+            text_features = torch.stack(text_features, dim=0)  # shape: [1, 768, 2]
 
             # sample
-            text_probs = (100.0 * image_features @ text_features[0]).softmax(dim=-1)
-            results['pr_sp'].append(text_probs[0][1].cpu().item())
+            text_probs = (100.0 * image_features @ text_features[0]).softmax(
+                dim=-1
+            )  # shape: [1, 2]
+
+            results["pr_sp"].append(text_probs[0][1].cpu().item())
 
             # pixel
             patch_tokens = linearlayer(patch_tokens)
             anomaly_maps = []
             for layer in range(len(patch_tokens)):
                 patch_tokens[layer] /= patch_tokens[layer].norm(dim=-1, keepdim=True)
-                anomaly_map = (100.0 * patch_tokens[layer] @ text_features)
+                anomaly_map = 100.0 * patch_tokens[layer] @ text_features
                 B, L, C = anomaly_map.shape
                 H = int(np.sqrt(L))
-                anomaly_map = F.interpolate(anomaly_map.permute(0, 2, 1).view(B, 2, H, H),
-                                            size=img_size, mode='bilinear', align_corners=True)
+                anomaly_map = F.interpolate(
+                    anomaly_map.permute(0, 2, 1).view(B, 2, H, H),
+                    size=img_size,
+                    mode="bilinear",
+                    align_corners=True,
+                )
                 anomaly_map = torch.softmax(anomaly_map, dim=1)[:, 1, :, :]
                 anomaly_maps.append(anomaly_map.cpu().numpy())
-            anomaly_map = np.sum(anomaly_maps, axis=0)
+            anomaly_map = np.sum(anomaly_maps, axis=0)  # nparray, [1, 518, 518]
 
             # few shot
-            if args.mode == 'few_shot':
-                image_features, patch_tokens = model.encode_image(image, few_shot_features)
+            # TODO: to understand later
+            if args.mode == "few_shot":
+                image_features, patch_tokens = model.encode_image(
+                    image, few_shot_features
+                )
                 anomaly_maps_few_shot = []
                 for idx, p in enumerate(patch_tokens):
-                    if 'ViT' in args.model:
+                    if "ViT" in args.model:
                         p = p[0, 1:, :]
                     else:
                         p = p[0].view(p.shape[1], -1).permute(1, 0).contiguous()
-                    cos = pairwise.cosine_similarity(mem_features[cls_name[0]][idx].cpu(), p.cpu())
+                    cos = pairwise.cosine_similarity(
+                        mem_features[cls_name[0]][idx].cpu(), p.cpu()
+                    )
                     height = int(np.sqrt(cos.shape[1]))
-                    anomaly_map_few_shot = np.min((1 - cos), 0).reshape(1, 1, height, height)
-                    anomaly_map_few_shot = F.interpolate(torch.tensor(anomaly_map_few_shot),
-                                                         size=img_size, mode='bilinear', align_corners=True)
+                    anomaly_map_few_shot = np.min((1 - cos), 0).reshape(
+                        1, 1, height, height
+                    )
+                    anomaly_map_few_shot = F.interpolate(
+                        torch.tensor(anomaly_map_few_shot),
+                        size=img_size,
+                        mode="bilinear",
+                        align_corners=True,
+                    )
                     anomaly_maps_few_shot.append(anomaly_map_few_shot[0].cpu().numpy())
                 anomaly_map_few_shot = np.sum(anomaly_maps_few_shot, axis=0)
                 anomaly_map = anomaly_map + anomaly_map_few_shot
-            
-            results['anomaly_maps'].append(anomaly_map)
+
+            results["anomaly_maps"].append(anomaly_map)
 
             # visualization
-            path = items['img_path']
-            cls = path[0].split('/')[-2]
-            filename = path[0].split('/')[-1]
-            vis = cv2.cvtColor(cv2.resize(cv2.imread(path[0]), (img_size, img_size)), cv2.COLOR_BGR2RGB)  # RGB
+            path = items["img_path"]
+            cls = path[0].split("/")[-2]  # good, broken_small, ...
+            filename = path[0].split("/")[-1]
+            vis = cv2.cvtColor(
+                cv2.resize(cv2.imread(path[0]), (img_size, img_size)), cv2.COLOR_BGR2RGB
+            )  # RGB
             mask = normalize(anomaly_map[0])
             vis = apply_ad_scoremap(vis, mask)
             vis = cv2.cvtColor(vis, cv2.COLOR_RGB2BGR)  # BGR
-            save_vis = os.path.join(save_path, 'imgs', cls_name[0], cls)
+            save_vis = os.path.join(save_path, "imgs", cls_name[0], cls)
             if not os.path.exists(save_vis):
                 os.makedirs(save_vis)
             cv2.imwrite(os.path.join(save_vis, filename), vis)
@@ -232,20 +313,24 @@ def test(args):
         pr_sp = []
         pr_sp_tmp = []
         table.append(obj)
-        for idxes in range(len(results['cls_names'])):
-            if results['cls_names'][idxes] == obj:
-                gt_px.append(results['imgs_masks'][idxes].squeeze(1).numpy())
-                pr_px.append(results['anomaly_maps'][idxes])
-                pr_sp_tmp.append(np.max(results['anomaly_maps'][idxes]))
-                gt_sp.append(results['gt_sp'][idxes])
-                pr_sp.append(results['pr_sp'][idxes])
+        for idxes in range(len(results["cls_names"])):
+            if results["cls_names"][idxes] == obj:
+                gt_px.append(results["imgs_masks"][idxes].squeeze(1).numpy())
+                pr_px.append(results["anomaly_maps"][idxes])
+                pr_sp_tmp.append(np.max(results["anomaly_maps"][idxes]))
+                gt_sp.append(results["gt_sp"][idxes])
+                pr_sp.append(results["pr_sp"][idxes])
         gt_px = np.array(gt_px)
         gt_sp = np.array(gt_sp)
         pr_px = np.array(pr_px)
         pr_sp = np.array(pr_sp)
-        if args.mode == 'few_shot':
+
+        # TODO: to understand later
+        if args.mode == "few_shot":
             pr_sp_tmp = np.array(pr_sp_tmp)
-            pr_sp_tmp = (pr_sp_tmp - pr_sp_tmp.min()) / (pr_sp_tmp.max() - pr_sp_tmp.min())
+            pr_sp_tmp = (pr_sp_tmp - pr_sp_tmp.min()) / (
+                pr_sp_tmp.max() - pr_sp_tmp.min()
+            )
             pr_sp = 0.5 * (pr_sp + pr_sp_tmp)
 
         auroc_px = roc_auc_score(gt_px.ravel(), pr_px.ravel())
@@ -257,7 +342,9 @@ def test(args):
         f1_scores = (2 * precisions * recalls) / (precisions + recalls)
         f1_sp = np.max(f1_scores[np.isfinite(f1_scores)])
         # f1_px
-        precisions, recalls, thresholds = precision_recall_curve(gt_px.ravel(), pr_px.ravel())
+        precisions, recalls, thresholds = precision_recall_curve(
+            gt_px.ravel(), pr_px.ravel()
+        )
         f1_scores = (2 * precisions * recalls) / (precisions + recalls)
         f1_px = np.max(f1_scores[np.isfinite(f1_scores)])
         # aupro
@@ -285,32 +372,83 @@ def test(args):
         ap_px_ls.append(ap_px)
 
     # logger
-    table_ls.append(['mean', str(np.round(np.mean(auroc_px_ls) * 100, decimals=1)),
-                     str(np.round(np.mean(f1_px_ls) * 100, decimals=1)), str(np.round(np.mean(ap_px_ls) * 100, decimals=1)),
-                     str(np.round(np.mean(aupro_ls) * 100, decimals=1)), str(np.round(np.mean(auroc_sp_ls) * 100, decimals=1)),
-                     str(np.round(np.mean(f1_sp_ls) * 100, decimals=1)), str(np.round(np.mean(ap_sp_ls) * 100, decimals=1))])
-    results = tabulate(table_ls, headers=['objects', 'auroc_px', 'f1_px', 'ap_px', 'aupro', 'auroc_sp',
-                                          'f1_sp', 'ap_sp'], tablefmt="pipe")
+    table_ls.append(
+        [
+            "mean",
+            str(np.round(np.mean(auroc_px_ls) * 100, decimals=1)),
+            str(np.round(np.mean(f1_px_ls) * 100, decimals=1)),
+            str(np.round(np.mean(ap_px_ls) * 100, decimals=1)),
+            str(np.round(np.mean(aupro_ls) * 100, decimals=1)),
+            str(np.round(np.mean(auroc_sp_ls) * 100, decimals=1)),
+            str(np.round(np.mean(f1_sp_ls) * 100, decimals=1)),
+            str(np.round(np.mean(ap_sp_ls) * 100, decimals=1)),
+        ]
+    )
+    results = tabulate(
+        table_ls,
+        headers=[
+            "objects",
+            "auroc_px",
+            "f1_px",
+            "ap_px",
+            "aupro",
+            "auroc_sp",
+            "f1_sp",
+            "ap_sp",
+        ],
+        tablefmt="pipe",
+    )
     logger.info("\n%s", results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("VAND Challenge", add_help=True)
     # paths
-    parser.add_argument("--data_path", type=str, default="./data/visa", help="path to test dataset")
-    parser.add_argument("--save_path", type=str, default='./results/tiaoshi', help='path to save results')
-    parser.add_argument("--checkpoint_path", type=str, default='./exps/vit_huge_14/model_epoch12.pth', help='path to save results')
-    parser.add_argument("--config_path", type=str, default='./open_clip/model_configs/ViT-B-16.json', help="model configs")
+    parser.add_argument(
+        "--data_path", type=str, default="./data/visa", help="path to test dataset"
+    )
+    parser.add_argument(
+        "--save_path",
+        type=str,
+        default="./results/tiaoshi",
+        help="path to save results",
+    )
+    parser.add_argument(
+        "--checkpoint_path",
+        type=str,
+        default="./exps/vit_huge_14/model_epoch12.pth",
+        help="path to save results",
+    )
+    parser.add_argument(
+        "--config_path",
+        type=str,
+        default="./open_clip/model_configs/ViT-B-16.json",
+        help="model configs",
+    )
     # model
-    parser.add_argument("--dataset", type=str, default='mvtec', help="test dataset")
+    parser.add_argument("--dataset", type=str, default="mvtec", help="test dataset")
     parser.add_argument("--model", type=str, default="ViT-B-16", help="model used")
-    parser.add_argument("--pretrained", type=str, default="laion400m_e32", help="pretrained weight used")
-    parser.add_argument("--features_list", type=int, nargs="+", default=[3, 6, 9], help="features used")
-    parser.add_argument("--few_shot_features", type=int, nargs="+", default=[3, 6, 9], help="features used for few shot")
+    parser.add_argument(
+        "--pretrained", type=str, default="laion400m_e32", help="pretrained weight used"
+    )
+    parser.add_argument(
+        "--features_list", type=int, nargs="+", default=[3, 6, 9], help="features used"
+    )
+    parser.add_argument(
+        "--few_shot_features",
+        type=int,
+        nargs="+",
+        default=[3, 6, 9],
+        help="features used for few shot",
+    )
     parser.add_argument("--image_size", type=int, default=224, help="image size")
-    parser.add_argument("--mode", type=str, default="zero_shot", help="zero shot or few shot")
+    parser.add_argument(
+        "--mode", type=str, default="zero_shot", help="zero shot or few shot"
+    )
     # few shot
-    parser.add_argument("--k_shot", type=int, default=10, help="e.g., 10-shot, 5-shot, 1-shot")
+    parser.add_argument(
+        "--k_shot", type=int, default=10, help="e.g., 10-shot, 5-shot, 1-shot"
+    )
     parser.add_argument("--seed", type=int, default=10, help="random seed")
     args = parser.parse_args()
 
